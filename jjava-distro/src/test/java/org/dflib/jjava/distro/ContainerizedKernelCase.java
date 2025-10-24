6d26c544d3fdc06debc140ecadc2c3ae506d7b14
Mikhail Dzianishchyts <mikhail.dzianishchyts@gmail.com>
2025-10-24 15:14:20 +0300

    End code snippet with EOL

--- jjava-distro/src/test/java/org/dflib/jjava/distro/ContainerizedKernelCase.java
+++ jjava-distro/src/test/java/org/dflib/jjava/distro/ContainerizedKernelCase.java
@@ -62,7 +62,8 @@ public abstract class ContainerizedKernelCase {
     }
 
     protected static Container.ExecResult executeInKernel(String snippet, Map<String, String> env) throws IOException, InterruptedException {
-        String snippet64 = Base64.getEncoder().encodeToString(snippet.getBytes());
+        String snippetEOL = snippet + "\n";
+        String snippet64 = Base64.getEncoder().encodeToString(snippetEOL.getBytes());
         String jupyterCommand = venvCommand("jupyter console --kernel=java --simple-prompt --no-confirm-exit -y");
         String[] containerCommand = new String[]{"bash", "-c", "echo \"" + snippet64 + "\" | base64 -d | " + jupyterCommand};
         Container.ExecResult execResult = container.execInContainer(ExecConfig.builder()
